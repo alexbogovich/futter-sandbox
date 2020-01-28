b3e230402b5ce1f64297f567fd57b0ec90629e43
Alex Bogovich <bogovich.alexander@gmail.com>
2020-01-28 16:42:36 +0700

    init chat app

---
+++ friendly_chat/lib/main.dart
@@ -0,0 +1,15 @@
+
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(
+    new MaterialApp(
+      title: "Friendlychat",
+      home: new Scaffold(
+        appBar: new AppBar(
+          title: new Text("Friendlychat"),
+        ),
+      ),
+    ),
+  );
+}
