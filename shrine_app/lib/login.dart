1e7cb871cb9ff389efe9238dcb9d7536bd28a854
Alex Bogovich <bogovich.alexander@gmail.com>
2020-01-28 21:05:44 +0700

    polish

--- shrine_app/lib/login.dart
+++ shrine_app/lib/login.dart
@@ -1,17 +1,3 @@
-// Copyright 2018-present the Flutter authors. All Rights Reserved.
-//
-// Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at
-//
-// http://www.apache.org/licenses/LICENSE-2.0
-//
-// Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and
-// limitations under the License.
-
 import 'package:flutter/material.dart';
 import 'colors.dart';
 
@@ -63,7 +49,6 @@ class _LoginPageState extends State<LoginPage> {
               ),
             ),
             ButtonBar(
-              // TODO: Add a beveled rectangular border to CANCEL (103)
               children: <Widget>[
                 FlatButton(
                   child: Text('CANCEL'),
@@ -75,8 +60,6 @@ class _LoginPageState extends State<LoginPage> {
                     borderRadius: BorderRadius.all(Radius.circular(7.0)),
                   ),
                 ),
-                // TODO: Add an elevation to NEXT (103)
-                // TODO: Add a beveled rectangular border to NEXT (103)
                 RaisedButton(
                   elevation: 8.0,
                   shape: BeveledRectangleBorder(
